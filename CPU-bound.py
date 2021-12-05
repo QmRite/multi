967e637019cddef7f8829978ddbe20f4e900526e
QmRite <82218195+QmRite@users.noreply.github.com>
2021-12-05 21:15:53 +0500

    Add files via upload

---
+++ CPU-bound.py
@@ -0,0 +1,13 @@
+from hashlib import md5
+from random import choice
+import concurrent.futures
+
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
+    while True:
+        s = "".join([choice("0123456789") for i in range(50)])
+        h = md5(s.encode('utf8')).hexdigest()
+
+        if h.endswith("00000"):
+            print(s, h)
+            break
